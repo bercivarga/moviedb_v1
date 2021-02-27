223627d970ac2926e6ae302c2bd434fe5c7c2a9d
vargaJS <65171545+vargaJS@users.noreply.github.com>
2021-02-27 13:19:50 +0100

    slider init

--- src/components/TrendingPage/TrendingMovies/TrendingMovies.js
+++ src/components/TrendingPage/TrendingMovies/TrendingMovies.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useGlobalContext } from '../../../context';
 import { Link } from 'react-router-dom';
+import Carousel, { slidesToScrollPlugin } from '@brainhubeu/react-carousel';
 
 import classes from './TrendingMovies.module.css';
 
